0eac5a7ca621731faa38e9fc571309f37ac8515c
Jerimiah Willhite <willhite.jerimiah@gmail.com>
2020-12-04 23:13:00 -0800

    Add the POWER OF TRANSLATION from Peter Olson

--- dec5/solution.py
+++ dec5/solution.py
@@ -7,12 +7,6 @@ ROWS = (0, 127)
 COLS = (0, 7)
 # L = lower half, R = upper half
 
-example = 'FBFBBFFRLR'
-example_rows = example[:7]
-example_cols = example[7:]
-
-print(example_rows)
-print(example_cols)
 class Ticket:
     def __init__(self, code, rows=ROWS, cols=COLS):
         self.code = code
@@ -41,13 +35,29 @@ class Ticket:
 
 
 with open('input.txt') as f:
-    input_list = f.read().split('\n')
+    input_list = f.read().strip().split('\n')
     tickets = [line for line in input_list]
 
 ticket_ids = [Ticket(ticket).id for ticket in tickets]
 print(min(ticket_ids))
 print(max(ticket_ids))
 
+missing = [tick for tick in range(min(ticket_ids), max(ticket_ids)) if tick not in ticket_ids]
 for ii in range(min(ticket_ids), max(ticket_ids)+1):
     if ii not in ticket_ids:
         print(ii)
+
+
+### The following is blatantly stolen from Peter Olson: https://github.com/Peter200lx/advent-of-code/blob/master/2020/day05.py
+from pathlib import Path
+
+FILE_DIR = Path(__file__).parent
+
+TRANSLATOR = "".maketrans("FLBR", "0011")
+
+if __name__ == "__main__":
+    DATA = (FILE_DIR / "input.txt").read_text().strip()
+    SEATS = {int(line.translate(TRANSLATOR), 2) for line in DATA.split("\n")}
+    max_seat = max(SEATS)
+    print(max_seat)
+    print([x for x in range(min(SEATS), max_seat) if x not in SEATS])
