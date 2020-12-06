f58916c30a4e01838109104233a7bd26f6506508
Jerimiah Willhite <willhite.jerimiah@gmail.com>
2020-12-05 21:36:48 -0800

    add pred's two-liner using set unions/intersections + mapping

--- dec6/solution.py
+++ dec6/solution.py
@@ -4,13 +4,17 @@ class Group(NamedTuple):
     answers: List
 
     def get_member_count(self):
-        pass
+        return len(self.answers)
+
     def get_count1(self) -> int:
         return len(set(''.join(self.answers)))
 
     def get_count2(self) -> int:
-        pass
-
+        answer_count = 0
+        for let in set(''.join(self.answers)):
+            if ''.join(self.answers).count(let) == self.get_member_count():
+                answer_count += 1
+        return answer_count
 
 
 with open('input.txt') as f:
@@ -18,4 +22,17 @@ with open('input.txt') as f:
 
 print(input_list)
 # sol 1
-print(sum([g.get_count() for g in input_list]))
+print(sum([g.get_count1() for g in input_list]))
+
+# sol 2
+print(sum([g.get_count2() for g in input_list]))
+
+'''
+### pred's two-liner:
+groups = data.split('\n\n')
+# Part one
+print(sum(len(set.union(*map(set, g.split('\n')))) for g in groups))
+
+# Part two
+print(sum(len(set.intersection(*map(set, g.split('\n')))) for g in groups))
+'''
