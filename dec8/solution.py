3a30d654bf34576c7c492479736b4ba29118058f
Jerimiah Willhite <willhite.jerimiah@gmail.com>
2020-12-07 23:37:38 -0800

    update partial solution with comments to be clearer

--- dec8/solution.py
+++ dec8/solution.py
@@ -1,12 +1,9 @@
-from typing import NamedTuple, List, Tuple, Dict
-from collections import defaultdict
-import re
+from typing import NamedTuple
 
 
 class Command(NamedTuple):
     command: str
     num: int
-
 
 class CPU:
     def __init__(self, filelength):
@@ -14,20 +11,19 @@ class CPU:
         self.pc = 0
         self.accumulator = 0
         self.ram = [None] * filelength
+        self.rebooted = False
 
+        # Table to call functions with
         self.branchtable = {
             'nop': self.nop,
             'jmp': self.jmp,
             'acc': self.acc
         }
 
-
     def load(self, file):
-        address = 0
-        # keeping track of the line of the command
-        for ii, command in enumerate(file):
-            self.ram[address] = (ii, self.parse_command(command))
-            address += 1
+        # keeping track of the address of the command
+        for address, command in enumerate(file):
+            self.ram[address] = (address, self.parse_command(command))
 
     def parse_command(self, command):
         c, i = command.split(" ")
@@ -46,15 +42,38 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        visited = set()
+        # we make this a list (or queue) so we can pop the previous command
+        stack = []
         while self.running:
             IR = self.ram[self.pc]
-            if IR[0] not in visited:
-                visited.add(IR[0])
+            if IR not in stack:
+                stack.append(IR)
                 self.branchtable[IR[1].command](IR[1].num)
-            else:
+            elif IR[0] == len(self.ram)-1:
+                print("We've finished! Final accumulator is: ")
                 print(self.accumulator)
                 self.running = False
+
+            elif self.rebooted is False:
+                print(self.accumulator)
+                # We get the previous command:
+                IR = stack.pop()
+                # overwrite command at ram address:
+                if IR[1].command == 'nop':
+                    self.ram[IR[0]] = (IR[0], Command('jmp', IR[1].num))
+                elif IR[1].command == 'jmp':
+                    self.ram[IR[0]] = (IR[0], Command('nop', IR[1].num))
+                # reset and go again (should only happen once)
+                self.pc = 0
+                self.accumulator = 0
+                stack = []
+                self.rebooted = True
+
+            else:
+                breakpoint()
+                print('bug')
+
+
 
 
 with open('input.txt') as f:
