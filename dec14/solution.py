c5fb289c8bf7e5efbf31be6cb7b0ec58f3760a1f
Jerimiah Willhite <willhite.jerimiah@gmail.com>
2020-12-13 23:34:19 -0800

    add commented solution for pt2 with own code

--- dec14/solution.py
+++ dec14/solution.py
@@ -7,12 +7,35 @@ class Address(NamedTuple):
     stored: List
 
     def sum_self(self):
+        # This should be working correctly
         return sum(int(x, 2) for x in self.stored)
 
-def apply_mask2(mask: str, instruction: str) -> List:
-    pass
+def apply_mask2(mask: str, instruction: str, *args) -> List:
+    #breakpoint()
+    if 'X' in mask:
+        output = Address(args[0], [''] * (2**mask.count('X')))
+        cutoff = len(output.stored)/2
+    else:
+        output = Address(args, [''])
+    for ii, let in enumerate(mask):
+        if let == 'X':
+            jj = 0
+            kk = 0
+            flip = False
+            while jj < len(output.stored):
+                while kk < cutoff:
+                    output.stored[jj] += str(int(flip))
+                    kk += 1
+                    jj += 1
+                kk = 0
+                flip = not flip
+            cutoff /= 2
+        else:
+            for x in range(len(output.stored)):
+                output.stored[x] += mask[ii]
+    return output
 
-def apply_mask(mask: str, instruction: str) -> str:
+def apply_mask(mask: str, instruction: str, *args) -> str:
     output = ''
     for ii, let in enumerate(mask):
         if let == 'X':
@@ -29,28 +52,65 @@ def generate_dict(instructions: List[str], mask_applier) -> dict:
         if line.startswith('mem'):
             inst = line.split("=")
             value = format(int(inst[1].strip()), '036b')
-            memory[inst[0].strip()[4:-1]] = mask_applier(mask, value)
+            memory[inst[0].strip()[4:-1]] = mask_applier(mask, value, inst[0].strip()[4:-1])
     return memory
 
 def sum_values(memory: dict) -> int:
     return sum([int(x, 2) for x in memory.values()])
 
-def sum_values2(memory: dict) -> int:
-    total = 0
-    for li in memory.values():
-        total += sum([int(x, 2) for x in l])
-    return total
-
-# create 36 places for every value of key in mem dict:
-
-# every time mask appears, update the method to translate.
-
 with open('input.txt') as f:
     instructions = [line for line in f.read().split('\n')]
 
-# sol1
+# sol1: answer is: 9628746976360
 mem = generate_dict(instructions, apply_mask)
-print(sum_values(mem))
+print('sol1: ', sum_values(mem))
 
-#sol2
-#mem2 = generate_dict(instructions, apply_mask2)
+#sol2: answer should be: 4574598714592
+mem2 = generate_dict(instructions, apply_mask2)
+print('sol2: ', sum([v.sum_self() for v in mem2.values()]))
+
+'''
+import collections
+import math
+import re
+import sys
+
+lines = instructions
+
+def domask(arg, mask):
+    arg |= int(mask.replace('X', '0'), 2)
+    arg &= int(mask.replace('X', '1'), 2)
+    return arg
+
+def allmasks(pos, mask):
+    if not mask:
+        yield 0
+    else:
+        # (yes, I probably could have done the ifs *inside* the loop...)
+        if mask[-1] == '0':
+            for m in allmasks(pos // 2, mask[:-1]):
+                yield 2*m + pos % 2
+        if mask[-1] == '1':
+            for m in allmasks(pos // 2, mask[:-1]):
+                yield 2*m + 1
+        if mask[-1] == 'X':
+            for m in allmasks(pos // 2, mask[:-1]):
+                yield 2*m + 0
+                yield 2*m + 1
+
+
+mask = None
+mem = collections.defaultdict(int)
+for line in lines:
+    op, arg = line.split(' = ')
+    if op == 'mask':
+        mask = arg
+    else:
+        pos = int(op[4:-1])
+        # part 1:
+        # mem[pos] = domask(int(arg), mask)
+        for m in allmasks(pos, mask):
+            mem[m] = int(arg)
+
+print(sum(mem.values()))
+'''
