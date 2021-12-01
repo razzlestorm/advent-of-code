98789dca0d2d555ec00b039c03021e266b942de8
Jerimiah Willhite <>
2021-11-30 22:11:02 -0800

    added list comprehension

--- 2021/dec01/solution.py
+++ 2021/dec01/solution.py
@@ -21,7 +21,6 @@ class WindowSolver:
             jj += 1
         return incs
 
-
 if __name__ == "__main__":
     DATA = (FILE_DIR / "input.txt").read_text().strip()
     data = [int(x) for x in DATA.split('\n')]
@@ -29,3 +28,14 @@ if __name__ == "__main__":
     sol2 = WindowSolver(3, data)
     print(sol1.count_increments())
     print(sol2.count_increments())
+
+    """
+    # Added for some fun list comprehension
+    def sliding_window_sums(input_list: List[int], window_size: int = 3) -> List[int]:
+        return sum([
+            (sum(input_list[i - window_size : i])) < (sum(input_list[i - window_size+1 : i+1]))
+            for i in range(window_size, len(input_list) + 1)
+        ])
+    print(sliding_window(data, 1))
+    print(sliding_window(data, 3))
+    """
