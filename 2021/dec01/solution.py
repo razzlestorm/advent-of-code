0e665d7f06f053b4b31f978ad98aef2ec5c9e79c
Jerimiah Willhite <>
2021-11-30 21:59:40 -0800

    second pass, TODO: split up function even more

--- 2021/dec01/solution.py
+++ 2021/dec01/solution.py
@@ -1,33 +1,31 @@
+from dataclasses import dataclass, field
 from pathlib import Path
 from typing import List
 
 FILE_DIR = Path(__file__).parent
 
-def solve1(input_list: List) -> int:
-    incs = 0
-    jj = 0
-    kk = 1
-    while kk < len(input_list):
-        if input_list[jj] < input_list[kk]:
-            incs += 1
-        jj += 1
-        kk += 1
-    return incs
 
-def solve2(input_list: List) -> int:
-    # implement sliding window of 3 nums
-    incs = 0
-    jj = 0
-    kk = 3
-    while kk < len(input_list):
-        if sum(input_list[jj:kk]) < sum(input_list[jj+1:kk+1]):
-            incs += 1
-        jj += 1
-        kk += 1
-    return incs
+@dataclass
+class WindowSolver:
+    window_size: int = 1
+    data: list[int] = field(default_factory=list)
+   
+    def count_increments(self):
+        incs = 0
+        ii = 0
+        jj = self.window_size
+        while jj < len(self.data):
+            if sum(self.data[ii:jj]) < sum(self.data[ii+1:jj+1]):
+                incs += 1
+            ii += 1
+            jj += 1
+        return incs
+
 
 if __name__ == "__main__":
     DATA = (FILE_DIR / "input.txt").read_text().strip()
     data = [int(x) for x in DATA.split('\n')]
-    print(solve1(data))
-    print(solve2(data))
+    sol1 = WindowSolver(1, data)
+    sol2 = WindowSolver(3, data)
+    print(sol1.count_increments())
+    print(sol2.count_increments())
