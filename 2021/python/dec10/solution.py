c88b53d46d738ce1a8fb8d9a3e922fcdf8f5184d
Jerimiah Willhite <willhite.jerimiah@gmail.com>
2021-12-09 22:31:08 -0800

    flake8 cleanup

--- 2021/python/dec10/solution.py
+++ 2021/python/dec10/solution.py
@@ -1,9 +1,8 @@
 from pathlib import Path
-from typing import List
-
 
 FILE_DIR = Path(__file__).parent
-POINTS = { 
+
+POINTS = {
     ")": 3,
     "]": 57,
     "}": 1197,
@@ -26,7 +25,9 @@ REV_PAIRS = {
 
 
 def find_corrupt(s: str) -> int:
-    "returns the points of the index that the corruption occurs in, or False if no corruption found"
+    """returns the points of the index that the corruption occurs in,
+    or False if no corruption found
+    """
     stack = []
     for ii, char in enumerate(s):
         if char in REV_PAIRS.values():
@@ -35,12 +36,13 @@ def find_corrupt(s: str) -> int:
             try:
                 left = stack.pop()
                 if REV_PAIRS[char] != left:
-                    #corresponding left character is wrong
+                    # corresponding left character is wrong
                     raise ValueError
             # can no longer pop off meaning no corresponding left char
             except ValueError:
                 return POINTS[char]
     return False
+
 
 def finish_line(s: str) -> int:
     "returns points for line finished"
@@ -49,7 +51,7 @@ def finish_line(s: str) -> int:
         if char in REV_PAIRS.values():
             stack.append(char)
         else:
-            left = stack.pop()
+            stack.pop()
     results = []
     for char in stack[::-1]:
         for k, v in REV_PAIRS.items():
@@ -62,7 +64,6 @@ def finish_line(s: str) -> int:
     return points
 
 
-
 if __name__ == "__main__":
 
     test_data = [
@@ -71,7 +72,7 @@ if __name__ == "__main__":
         "{([(<{}[<>[]}>{[]{[(<()>",
         "(((({<>}<{<{<>}{[]{[]{}",
         "[[<[([]))<([[{}[[()]]]",
-       "[{[{({}]{}}([{[{{{}}([]",
+        "[{[{({}]{}}([{[{{{}}([]",
         "{<[[]]>}<{[{[{[]{()[[[]",
         "[<(<(<(<{}))><([]([]()",
         "<{([([[(<>()){}]>(<<{{",
@@ -79,16 +80,15 @@ if __name__ == "__main__":
     ]
 
     test_outputs = [x for x in test_data]
-    print(sum([find_corrupt(x) for x in test_outputs])) # 26397
-    print(sum([finish_line(x) for x in test_data if not find_corrupt(x)])) # 2771042
+    print(sum([find_corrupt(x) for x in test_outputs]))  # 26397
+    print(sum([finish_line(x) for x in test_data if not find_corrupt(x)]))  # 2771042
 
     DATA = (FILE_DIR / "input.txt").read_text().strip()
     data = [x for x in DATA.split("\n")]
     # sol 1
-    print(sum([find_corrupt(x) for x in data])) # 265527
+    print(sum([find_corrupt(x) for x in data]))  # 265527
 
     # sol 2
     sol2 = [finish_line(x) for x in data if not find_corrupt(x)]
     sol2 = sorted(sol2)
-    print(sol2[len(sol2)//2]) # 3969823589
-
+    print(sol2[len(sol2)//2])  # 3969823589
