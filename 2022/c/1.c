ee4cf09cf1be7ea7925f9db31a5a2dc83de3a9c4
Jerimiah Willhite <willhite.jerimiah@gmail.com>
2023-10-19 23:38:36 -0700

    complete day 1 both parts

--- 2022/c/1.c
+++ 2022/c/1.c
@@ -2,6 +2,32 @@
 #include <stdio.h>
 
 int main(){
-	printf("hi\n");
-	return 0;
+
+	char stringBuf[64];
+	long max[3] = {0, 0, 0};
+	long sum = 0;
+	FILE* ptr;
+	ptr = fopen("1.txt", "r");
+	if (ptr == NULL){
+		printf("file can't be opened\n");
+	}
+
+	while (fgets(stringBuf, 64, ptr)){
+		if (stringBuf[0] == '\n'){
+			for (int i = 2; i >=0; i--){
+				if (sum >= max[i]){
+					long tmp = max[i];
+					max[i] = sum;
+					sum = tmp;
+				}
+			}
+			sum = 0;
+		}
+		sum += strtol(stringBuf, NULL, 10);
+	}
+
+	fclose(ptr);
+	printf("Top three are: %d, %d, %d\n", max[2], max[1], max[0]);
+	printf("Their sum is: %d\n", max[0]+max[1]+max[2]);
+	return 0; 
 }
