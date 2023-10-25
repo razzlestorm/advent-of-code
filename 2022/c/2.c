ac27522d2861e4ad54c949e651e9d951a6b7fa94
Jerimiah Willhite <willhite.jerimiah@gmail.com>
2023-10-24 20:41:49 -0700

    second part for 2.c still ugly

--- 2022/c/2.c
+++ 2022/c/2.c
@@ -4,6 +4,8 @@
 
 int score_line(char opp, char player);
 
+int score_line_two(char opp, char player);
+
 void test_func(){
 	int total = 0;
 	total = score_line('A', 'Y');
@@ -12,6 +14,13 @@ void test_func(){
 	printf("%d should equal 15\n", total);
 }
 
+void test_func_two(){
+	int total = 0;
+	total = score_line_two('A', 'Y');
+	total += score_line_two('B', 'X');
+	total += score_line_two('C', 'Z');
+	printf("%d should equal 12\n", total);
+}
 int score_line(char opp, char player){
 	int total = 0;
 	switch (player) {
@@ -73,6 +82,67 @@ int score_line(char opp, char player){
 	return total;
 }
 
+
+int score_line_two(char opp, char player){
+	int total = 0;
+	switch (player) {
+		case 'X':
+			switch (opp){
+				case 'A':
+				total += 3;
+				break;
+			}
+			switch (opp){
+				case 'B':
+				total += 1;
+				break;
+			}
+			switch (opp){
+				case 'C':
+				total += 2;
+				break;
+			}
+			break;
+		case 'Y':
+			total += 3;
+			switch (opp){
+				case 'A':
+				total += 1;
+				break;
+			}
+			switch (opp){
+				case 'B':
+				total += 2;
+				break;
+			}
+			switch (opp){
+				case 'C':
+				total += 3;
+				break;
+			}
+			break;
+		case 'Z':
+			total += 6;
+			switch (opp){
+				case 'A':
+				total += 2;
+				break;
+			}
+			switch (opp){
+				case 'B':
+				total += 3;
+				break;
+			}
+			switch (opp){
+				case 'C':
+				total += 1;
+				break;
+			}
+			break;
+	}
+	return total;
+}
+
 int main(){
 
 	char line[8];
@@ -84,12 +154,12 @@ int main(){
 
 	int total = 0;
 	while (fgets(line, 8, ptr)){
-		total += score_line(line[0], line[2]);
+		total += score_line_two(line[0], line[2]);
 	}
 
 	fclose(ptr);
-	test_func();
+	test_func_two();
 
-	printf("Total according to strategy guide: %d", total);
+	printf("%d", total);
 	return 0;
 }
