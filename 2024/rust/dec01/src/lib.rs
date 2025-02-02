058d2cf7858fa8cd88f461f1e501654dacb4071d
Jerimiah Willhite <willhite.jerimiah@gmail.com>
2025-02-01 22:17:32 -0800

    create solution for part 1

---
+++ 2024/rust/dec01/src/lib.rs
@@ -0,0 +1,31 @@
+use std::fs;
+use std::error::Error;
+
+
+pub fn part1(input: &str) -> u32 {
+    // Create two vecs for numbers on either side
+    let (mut left, mut right): (Vec<u32>, Vec<u32>) = input.lines()
+        .map(|line| {
+            let n: Vec<u32> = line 
+                .split_whitespace()
+                .map(|n| n.parse().ok().unwrap())
+                .collect();
+            (n[0], n[1])
+        })
+        .unzip();
+
+    // sort them
+    left.sort();
+    right.sort();
+
+    // iter over both and get the abs_diff, then sum everything
+    left.iter()
+        .zip(right)
+        .map(|(l, r)| l.abs_diff(r))
+        .sum()
+}
+
+pub fn load_input(path: &str) -> Result<String, Box<dyn Error>> {
+    let message: String = fs::read_to_string(path)?;
+    Ok(message)
+}
--- 2024/rust/dec01/src/main.rs
+++ 2024/rust/dec01/src/main.rs
@@ -1,3 +1,9 @@
+use dec01::{load_input, part1};
+
+
 fn main() {
-    println!("Hello, world!");
+    let lines = load_input("./src/input.txt").expect("File doesn't exist");
+    let answer1 = part1(&lines);
+    println!("answer 1 : {:?}", answer1);
+
 }
