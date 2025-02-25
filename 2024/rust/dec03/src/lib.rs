54a03d0ac769c2e9a83a77aa9b76dc7c778164bf
Jerimiah Willhite <willhite.jerimiah@gmail.com>
2025-02-24 23:16:17 -0800

    solve part 1

--- 2024/rust/dec03/src/lib.rs
+++ 2024/rust/dec03/src/lib.rs
@@ -1,11 +1,19 @@
 use std::fs;
 use std::error::Error;
+use regex::Regex;
 
 
 pub fn part1(input: &str) -> u32 {
     // create a function to scan string, look fro only mul(\d[1-3],\d[1-3]) I guess, then
     // multiply and add those all up?
-    1
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    let mut result = 0;
+    for (_, [num1, num2]) in re.captures_iter(input).map(|cg| cg.extract()) {
+        let m = num1.parse::<u32>().unwrap() * num2.parse::<u32>().unwrap();
+        result += m;
+    }
+    result
+
 }
 
 
